99833f62bb48fe6c328e396f16c5facc1fca5506
Chris Borbidge <chris@elkthelabel.com>
2020-01-17 10:43:47 +1100

    Update app.js

--- static/js/app.js
+++ static/js/app.js
@@ -1,7 +1,7 @@
 
 $(document).ready(function(){
     //connect to the socket server.
-    var socket = io.connect('http://' + document.domain + ':' + location.port + '/random');
+    var socket = io.connect('https://' + document.domain + ':' + location.port + '/random');
     var numbers_received = [];
 
     //receive details from server
